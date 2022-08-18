a173f8bf269e19b968f09009768271884914dfde
Perapol <peerapolth3327@hotmail.com>
2022-08-18 15:03:36 +0700

    finished

--- components/Todo.jsx
+++ components/Todo.jsx
@@ -1,11 +1,19 @@
-import React, { useState } from "react";
-import {
-  IconCheck,
-  IconTrash,
-  IconArrowUp,
-  IconArrowDown,
-} from "@tabler/icons";
+import React, { useEffect, useRef, useState } from "react";
+import Menu from "./Menu";
 
-export default function Todo(props) {
-  return <div>your code here...</div>;
+export default function Todo({ title, completed, on }) {
+  // const element = useRef();
+  const [menuHide, setMenuHide] = useState(true);
+  // useEffect(() => {
+  // }, [element])
+  return (
+    <div
+      className="border-bottom p-1 py-2 fs-2 d-flex gap-2"
+      onMouseOver={() => setMenuHide(false)}
+      onMouseOut={() => setMenuHide(true)}
+    >
+      <span className="me-auto" style={{ textDecoration: completed ? 'line-through' : '' }}>{title}</span>
+      <Menu on={on} hide={menuHide}></Menu>
+    </div >
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,92 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Todo from "../components/Todo";
-import {
-  IconCheck,
-  IconTrash,
-  IconArrowUp,
-  IconArrowDown,
-} from "@tabler/icons";
 
 export default function Home() {
-  const deleteTodo = (idx) => {};
+  const [started, setStarted] = useState(false);
+  const [todos, setTodos] = useState([]);
+  const [counter, setCounter] = useState({ completed: 0, pending: 0 });
+  const [todoElements, setTodoElements] = useState([]);
+  const deleteTodo = (idx) => {
+    setTodos(todos.filter((_, i) => {
+      return i !== idx;
+    }));
+  };
 
-  const markTodo = (idx) => {};
+  const markTodo = (idx) => {
+    setTodos(todos.map((element, i) => {
+      if (i === idx)
+        return { title: element.title, completed: !element.completed };
+      return element;
+    }));
+  };
 
-  const moveUp = (idx) => {};
+  const moveUp = (idx) => {
+    if (idx === 0 || todos.length <= 1) return;
+    const newTodos = todos.map(x => x);
+    const temp = newTodos[idx];
+    newTodos[idx] = newTodos[idx - 1];
+    newTodos[idx - 1] = temp;
+    setTodos(newTodos)
+  };
 
-  const moveDown = (idx) => {};
+  const moveDown = (idx) => {
+    if (idx === todos.length - 1 || todos.length <= 1) return;
+    const newTodos = todos.map(x => x);
+    const temp = newTodos[idx];
+    newTodos[idx] = newTodos[idx + 1];
+    newTodos[idx + 1] = temp;
+    setTodos(newTodos)
+  };
+
+  const addTodo = (val) => setTodos([{ title: val, completed: false }, ...todos])
+
+
+  const onInput = (e) => {
+    if (e.key !== 'Enter') return;
+    if (e.target.value == '') {
+      alert('Todo cannot be empty!');
+      return;
+    }
+    addTodo(e.target.value);
+    e.target.value = '';
+  }
+
+  const localKey = 'todos';
+  const localSaveTodo = () => {
+    if (typeof window === 'undefined') return;
+    window.localStorage.setItem(localKey, JSON.stringify(todos));
+  }
+  const localLoadTodo = () => {
+    if (typeof window === 'undefined') return;
+    setTodos(JSON.parse(window.localStorage.getItem(localKey)));
+  }
+
+  useEffect(() => {
+    if (!started) return;
+    localSaveTodo();
+    const elements = [];
+    const newCounter = { completed: 0, pending: 0 };
+    setTodoElements([]);
+    for (let i = 0; i < todos.length; i++) {
+      elements.push(<Todo
+        key={i} title={todos[i].title} completed={todos[i].completed}
+        on={{
+          complete: () => markTodo(i),
+          up: () => moveUp(i),
+          down: () => moveDown(i),
+          delete: () => deleteTodo(i)
+        }
+        }></Todo >);
+      newCounter.completed += todos[i].completed ? 1 : 0;
+      newCounter.pending += todos[i].completed ? 0 : 1;
+    }
+    setTodoElements(elements)
+    setCounter(newCounter)
+  }, [todos, started]);
+  useEffect(() => {
+    setStarted(true);
+    localLoadTodo()
+  }, []);
 
   return (
     <div>
@@ -28,42 +100,23 @@ export default function Home() {
         <input
           className="form-control mb-1 fs-4"
           placeholder="insert todo here..."
+          onKeyDown={onInput}
         />
         {/* Todos */}
-        {/* Example 1 */}
-        <div className="border-bottom p-1 py-2 fs-2 d-flex gap-2">
-          <span className="me-auto">Todo</span>
-        </div>
-        {/* Example 2 */}
-        <div className="border-bottom p-1 py-2 fs-2 d-flex gap-2">
-          <span className="me-auto">Todo with buttons</span>
-
-          <button className="btn btn-success">
-            <IconCheck />
-          </button>
-          <button className="btn btn-secondary">
-            <IconArrowUp />
-          </button>
-          <button className="btn btn-secondary">
-            <IconArrowDown />
-          </button>
-          <button className="btn btn-danger">
-            <IconTrash />
-          </button>
-        </div>
+        <div>{todoElements}</div>
 
         {/* summary section */}
         <p className="text-center fs-4">
-          <span className="text-primary">All (2) </span>
-          <span className="text-warning">Pending (2) </span>
-          <span className="text-success">Completed (0)</span>
+          <span className="text-primary">All ({counter.completed + counter.pending}) </span>
+          <span className="text-warning">Pending ({counter.pending}) </span>
+          <span className="text-success">Completed ({counter.completed})</span>
         </p>
 
         {/* Made by section */}
         <p className="text-center mt-3 text-muted fst-italic">
-          made by Chayanin Suatap 12345679
+          made by Perapol Pamoonchaer 640610655
         </p>
       </div>
-    </div>
+    </div >
   );
 }
